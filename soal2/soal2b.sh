b5408de7292ec9085c6f16987eb7dacf59e4a7de
stchffh <58497911+sittichofifah@users.noreply.github.com>
2020-02-21 07:11:07 +0700

    Create soal2b.sh

---
+++ soal2/soal2b.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+key=$(date +"%H")
+filename=$1
+file="${filename%.*}"
+
+low=abcdefghijklmnopqrstuvwxyz
+low=$low$low
+upper=ABCDEFGHIJKLMNOPQRSTUVWXYZ
+upper=$upper$upper
+
+updatefile=`printf "$file"  | tr [${low:26}${upper:26}] [${low:$key:26}${upper:$key:26}]`
+
+mv $file.txt $updatefile.txt
